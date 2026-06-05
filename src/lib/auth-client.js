01efe3bdb450bda2ec71eb749b9ffbd6ab5d9f6a
Rahim-Ahmed-10 <rahimahmed01690@gmail.com>
2026-06-06 02:30:37 +0600

    update for auth-client.js

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -3,7 +3,7 @@ import { createAuthClient } from 'better-auth/react';
 
 // ১. কনফিগারেশনসহ মূল authClient তৈরি করুন
 export const authClient = createAuthClient({
-  baseURL: 'http://localhost:3000',
+  baseURL: process.env.BETTER_AUTH_URL || process.env.NEXT_PUBLIC_BETTER_AUTH_URL, 
   plugins: [
     jwtClient(), 
   ]
