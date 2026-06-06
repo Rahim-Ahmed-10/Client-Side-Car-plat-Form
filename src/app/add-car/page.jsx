308043f2260cd81e24efd37257c8c51a5284337b
Rahim-Ahmed-10 <rahimahmed01690@gmail.com>
2026-06-06 11:54:05 +0600

    Update File Page

---
+++ src/app/add-car/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddCardPage = () => {
+    return (
+        <div>
+            Hello from add card page
+        </div>
+    );
+};
+
+export default AddCardPage;
---
+++ src/app/file/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+filepage
+        </div>
+    );
+};
+
+export default page;
